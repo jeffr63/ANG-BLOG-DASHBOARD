d1a07a26c7f8b5851e31949bed381b222b2131dd
jeffr63 <jeffr63@gmail.com>
2023-01-20 10:22:07 -0600

    At 3:04:30

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import AppComponent from './app/app.component';
 import { APP_ROUTES } from './app/app.routing';
+import { environment } from './environments/environment.prod';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore())
+    ),
     provideAnimations(),
     provideHttpClient(),
     provideRouter(APP_ROUTES),
   ],
 }).catch((err) => console.error(err));
-
