20e94e06dba19bf71ebc681a752f130e9760ea2d
jeffr63 <jeffr63@gmail.com>
2023-01-24 13:20:53 -0600

    At 3:28

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,34 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   template: `
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
-          <h3 class="text-center">New Categories</h3>
+        <div class="col-md-12 bg-secondary-theme p-5 shadow-effect">
+          <h3 class="text-center text-theme-primary">New Categories</h3>
           <p class="text-center mb-5">You can add new categories from here!</p>
 
-          <form class="form-inline.text-center">
-            <div class="form-group col-md-10">
+          <form
+            class="form-inline text-center"
+            #categoryForm="ngForm"
+            (ngSubmit)="onSubmit(categoryForm)"
+          >
+            <div class="col-md-10">
               <input
-                tyle="text"
+                type="text"
+                name="category"
                 class="form-control shadow-effect"
                 placeholder="Add New Category"
+                ngModel
               />
             </div>
 
             <div class="col-md-2">
-              <button class="btn btn-block btn-info mb-2">Add Category</button>
+              <button class="btn btn-block btn-info mb-2 button-theme">
+                Add Category
+              </button>
             </div>
           </form>
         </div>
       </div>
     </div>
   `,
-  styles: [],
+  styles: [
+    `
+      input[type='text'] {
+        width: 100%;
+      }
+    `,
+  ],
 })
-export default class CategoriesComponent {}
+export default class CategoriesComponent {
+  onSubmit(formData: any) {
+    let categoryData = {
+      category: formData.value.category,
+    };
+    console.log(categoryData);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import AppComponent from './app/app.component';
 import { APP_ROUTES } from './app/app.routing';
@@ -12,10 +10,6 @@ import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-      provideFirestore(() => getFirestore())
-    ),
     provideAnimations(),
     provideHttpClient(),
     provideRouter(APP_ROUTES),
